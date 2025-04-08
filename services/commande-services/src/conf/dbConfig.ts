a434bca51eebb58dac36d2edcd6d31b70493dcbe
nlecossois <lecossois.nolan@gmail.com>
2025-04-08 09:02:27 +0200

    Initialisation du service commande

---
+++ services/commande-services/src/conf/dbConfig.ts
@@ -0,0 +1,32 @@
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dbOption = {
+  db: process.env.DB_NAME || "postgres",
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "password",
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 5432,
+};
+
+export const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: dbOption.host,
+  username: dbOption.user,
+  password: dbOption.password,
+  database: dbOption.db,
+  port: dbOption.port,
+  models: [],
+  logging: false,  
+});
+
+export const initializeDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Connexion à la base de données réussie.");
+  } catch (error) {
+    console.error("❌ Erreur de connexion à la base de données:", error);
+  }
+};
