c89a551e659193d67174c46bbf58ab216add5659
nlecossois <lecossois.nolan@gmail.com>
2025-04-09 14:09:58 +0200

    Patch structure

--- services/restaurant-service/src/routes/restaurant.routes.ts
+++ services/restaurant-service/src/routes/restaurant.routes.ts
@@ -2,7 +2,7 @@ import restaurantController from "../controller/restaurant.controller";
 import {Application} from "express";
 
 export default function registerRestaurantRoutes(app: Application) {
-    app.post("/restaurants/createRestaurant", restaurantController.createRestaurant);
+    app.post("/restaurants/", restaurantController.createRestaurant);
     app.get("/restaurants/:id", restaurantController.getRestaurant);
     app.get("/getAllRestaurants", restaurantController.getAllRestaurants);
     app.put("/restaurants/:id", restaurantController.updateRestaurant);
